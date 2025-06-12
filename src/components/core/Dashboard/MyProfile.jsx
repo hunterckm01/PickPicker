0b962c438dd73974ef8c1492ca3da7044e9da4a8
hunterckm01 <huner6894@gmail.com>
2025-06-12 10:26:32 +0530

    Updated Photos preview, Added Delete Folder

--- src/components/core/Dashboard/MyProfile.jsx
+++ src/components/core/Dashboard/MyProfile.jsx
@@ -20,10 +20,8 @@ const MyProfile = () => {
   },[clients])
 
   return (
-    <div>
-      
-    </div>
-  )
+    <div className="w-screen min-h-[calc(100vh-131px)] bg-[linear-gradient(103deg,_#efdaffc0_2.13%,_#b458ff8f_104.3%)]"></div>
+  );
 }
 
 export default MyProfile
